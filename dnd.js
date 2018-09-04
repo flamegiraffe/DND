be467d84e3a169c74b92ce31d420138330451b8e
Dani Demeter <flamegiraffes@gmail.com>
2018-09-04 21:23:26 +0200

    highlighting, moving

--- dnd.js
+++ dnd.js
@@ -4,12 +4,14 @@ var gridSpacing = 50;
 var zoomSpeed = 1;
 var xOff = 0;
 var yOff = 0;
-var white;
-var black;
-var gray;
+var white, black, gray, blue;
 var hX, hY;
-var isHigh;
 var barbImg;
+var hiLi = {
+   isHigh: false,
+   posX: 0,
+   posY: 0
+};
 
 var characters = [];
 
@@ -18,8 +20,6 @@ function preload(){
 }
 
 function setup() {
-   print("changed");
-
    setupColors();
    setupChars();
    cnv = createCanvas(windowWidth, windowHeight);
@@ -27,14 +27,20 @@ function setup() {
 }
 
 function setupChars(){
-   var barb = new dndchar(barbImg, 10, 5);
-   characters.push(barb);
+   // var barb = new dndchar(barbImg, 10, 5);
+   // characters.push(barb);
+   characters.push({
+      image: barbImg,
+      posX: 10,
+      posY: 5
+   });
 }
 
 function setupColors(){
    white = color(245, 240, 246);
    black = color(28, 29, 32);
    gray = color(220, 220, 220);
+   blue = color(42, 183, 202);
 }
 
 function moveGrid(){
@@ -58,24 +64,51 @@ function drawGrid(){
 }
 
 function drawChars(){
+   // for(var i=0; i<characters.length; i++){
+   //    image(characters[i].getImage(), (characters[i].getPosX()+xOff)*gridSpacing, (characters[i].getPosY()+yOff)*gridSpacing, gridSpacing, gridSpacing);
+   // }
    for(var i=0; i<characters.length; i++){
-      image(characters[i].getImage(), (characters[i].getPosX()+xOff)*gridSpacing, (characters[i].getPosY()+yOff)*gridSpacing, gridSpacing, gridSpacing);
+      image(characters[i].image, (characters[i].posX+xOff)*gridSpacing, (characters[i].posY+yOff)*gridSpacing, gridSpacing, gridSpacing);
+   }
+}
+function drawHiLi(){
+   if(hiLi.isHigh){
+      stroke(blue);
+      strokeWeight(2);
+      fill(white);
+      rect((hiLi.posX+xOff)*gridSpacing, (hiLi.posY+yOff)*gridSpacing, gridSpacing, gridSpacing);
+      strokeWeight(1);
    }
 }
 function redrawAll(){
    clear();
    background(white);
    drawGrid();
+   drawHiLi();
    drawChars();
 }
 function mousePressed(){
    var gx = Math.floor(mouseX/gridSpacing)-xOff;
    var gy = Math.floor(mouseY/gridSpacing)-yOff;
+   var clickedOnChar = false;
    for(var i=0; i<characters.length; i++){
-      if(gx == characters[i].getPosX() && gy == characters[i].getPosY()){
-         // isHigh = true;
+      if(gx == characters[i].posX && gy == characters[i].posY){
+         clickedOnChar = true;
+         hiLi.posX = gx;
+         hiLi.posY = gy;
+         hiLi.isHigh = true;
       }
    }
+   if(!clickedOnChar){
+      for(var i=0; i<characters.length; i++){
+         if(hiLi.posX == characters[i].posX && hiLi.posY == characters[i].posY){
+            hiLi.isHigh = false;
+            characters[i].posX = gx;
+            characters[i].posY = gy;
+         }
+      }
+   }
+   redrawAll();
 }
 function keyPressed(){
    print(keyCode);
